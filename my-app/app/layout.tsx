604abe955a6f83ee5f5175f803d19824e04786f1
pranav-cheraku <pranavcheraku@gmail.com>
2026-03-16 00:30:21 -0700

    Added vercel analytics and speed insights

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { T } from "@/lib/theme";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Load Playfair Display
 const playfair = Playfair_Display({
@@ -76,6 +78,8 @@ export default function RootLayout({
 
         {/* Footer renders on every page */}
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
